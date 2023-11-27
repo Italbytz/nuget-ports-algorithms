65faa1a99cf7f422c3b007a18e1018707a3afeaf
nunkesser <robin.nunkesser@hshl.de>
2023-11-27 10:54:58 +0100

    AIMA

---
+++ Italbytz.Ports.Algorithms/Italbytz.Ports.Algorithms/IAgent.cs
@@ -0,0 +1,14 @@
+﻿/** 
+ * The original version of this file is part of <see href="https://github.com/aimacode/aima-java"/> which is released under 
+ * MIT License
+ * Copyright (c) 2015 aima-java contributors
+ */
+
+using System;
+namespace Italbytz.Ports.Algorithms
+{
+    public interface IAgent<TPercept, TAction>
+    {
+    }
+}
+
