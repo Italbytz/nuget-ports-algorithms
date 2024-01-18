c0e4fccd076334ab46e225e8e5f89938ab1c51ca
nunkesser <robin.nunkesser@hshl.de>
2024-01-18 09:59:03 +0100

    0.3.2

--- Italbytz.Ports.Algorithms/Italbytz.Ports.Algorithms/AI/Search/Continuous/LPFileFormat.cs
+++ Italbytz.Ports.Algorithms/Italbytz.Ports.Algorithms/AI/Search/Continuous/LPFileFormat.cs
@@ -2,6 +2,6 @@ namespace Italbytz.Ports.Algorithms.AI.Search.Continuous
 {
     public enum LPFileFormat
     {
-        lp_solve, MPS
+        lp_solve, MPS_FIXED, MPS_FREE, MPS_IBM, MPS_NEGOBJCONST
     }
 }
